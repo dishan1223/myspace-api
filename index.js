3964065f626306d60151c56c9c3990a1242fd391
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-01-25 16:49:31 +0600

    added json web token

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/authRouter'); // Correct path
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Fixed PORT declaration
+const PORT = process.env.PORT || 4000; // Fixed PORT declaration
 
 // Middleware
 app.use(bodyParser.json());
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const auth = require('../handler/auth'); // Import auth module
 const authRouter = express.Router();
+
+// JWT secret key
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authenticateToken = (req, res, next) => {
+    // bearer TOKEN
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if(!token){
+        res.status(401).send('Access Denied');
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) {
+            res.status(403).send('Invalid token');
+        } else {
+            req.user = user;
+            next();
+        }
+    });
+}
 
 // Fetch all users (for testing purposes)
 authRouter.get('/user', async (req, res) => {
